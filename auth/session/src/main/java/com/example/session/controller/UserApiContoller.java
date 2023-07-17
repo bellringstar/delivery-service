e705da1c35fca0c18a29444edd0956376253c6d8
bellring <starringbell@gmail.com>
2023-07-17 22:48:25 +0900

    세션에 저장된 유저 정보를 보여주는 api

---
+++ auth/session/src/main/java/com/example/session/controller/UserApiContoller.java
@@ -0,0 +1,24 @@
+package com.example.session.controller;
+
+import com.example.session.model.UserDto;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiContoller {
+
+    @GetMapping("/me")
+    public UserDto me(HttpSession httpSession){
+        var userObject =  httpSession.getAttribute("USER");
+        if (userObject != null){
+            return (UserDto) userObject;
+        }
+        else {
+            return null;
+        }
+    }
+
+}
