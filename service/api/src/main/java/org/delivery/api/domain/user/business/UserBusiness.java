3077cc7a572c427385178cd0877d8d4db94002ab
bellring <starringbell@gmail.com>
2023-07-22 22:48:07 +0900

    Feat : 로그인 시 토큰을 response 해주도록 변경

--- service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
 import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.business.TokenBusiness;
+import org.delivery.api.domain.token.controller.model.TokenResponse;
 import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
@@ -18,6 +20,7 @@ public class UserBusiness {
 
     private final UserService userService;
     private final UserConverter userConverter;
+    private final TokenBusiness tokenBusiness;
 
     public UserResponse register(UserRegisterRequest request) {
 //        var userEntity = userConverter.toEntity(request);
@@ -32,9 +35,10 @@ public class UserBusiness {
 
     }
 
-    public UserResponse login(UserLoginRequest request) {
+    public TokenResponse login(UserLoginRequest request) {
         var userEntity = userService.login(request.getEmail(), request.getPassword());
         //TODO 토큰생성로직 추가
-        return userConverter.toResponse(userEntity);
+        var tokenResponse = tokenBusiness.issueToken(userEntity);
+        return tokenResponse;
     }
 }
